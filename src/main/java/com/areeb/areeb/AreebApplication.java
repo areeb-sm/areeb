60add3b50c2935637611043ec84f978c1c70a658
S M Areeb <smareeb@ymail.com>
2018-08-31 14:48:56 +0530

    initial setup

---
+++ src/main/java/com/areeb/areeb/AreebApplication.java
@@ -0,0 +1,12 @@
+package com.areeb.areeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/areeb/areeb/AreebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.areeb.areeb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AreebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
